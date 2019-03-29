9a7026827f6e06476ee80a56ccce8ffad8072cc6
unknown <fdsa4321jkl@yahoo.com>
2019-03-29 15:31:03 -0700

    still needs a lot of work

--- src/App.js
+++ src/App.js
@@ -1,59 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios'
-import Dashboard from './components/Dashboard/Dashboard'
-import Product from './components/Product/Product'
-import Form from './components/Form/Form'
-import Header from './components/Header/Header'
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Product from "./components/Product/Product";
+import Form from "./components/Form/Form";
+import Header from "./components/Header/Header";
+import { HashRouter, Route, Link, Switch } from "react-router-dom";
+import routes from "./routes";
 
 // let productsArr = [{name:'oxford shirt', price: 30, imgURL: ''},{name: 'leather boots', price: 150, imgURL: ''},{name: 'leather jacket', price: 250, imgURL: ''}]
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-        products: [],
-        editId: null
-    }
-}
+      products: [],
+      editId: null
+    };
+  }
 
-displayInventory = ()=>{
-  axios.get('/api/inventory').then(res=>{
-    console.log(res)
-    // this.setState({
-    //   products: res.data
-    // });
-    // console.log(this.state.products)
-  }).catch(err => console.log(`OHMYGOD: ${err}`))
-}
+  displayInventory = () => {
+    this.componentDidMount();
+  };
 
-selectedProduct=(id)=>{
-  this.setState({
-    editId: id
-  })
-}
-
-componentDidMount(){
-  axios.get('/api/inventory').then(res=>{
-    console.log(res);
+  selectedProduct = id => {
     this.setState({
-      products: res.data
+      editId: id
     });
-    console.log(this.state.products)
-  }).catch(err => console.log(`OHMYGOD: ${err}`))
-}
+  };
 
+  componentDidMount() {
+    axios
+      .get("/api/inventory")
+      .then(res => {
+        console.log(res);
+        this.setState({
+          products: res.data
+        });
+        console.log(this.state.products);
+      })
+      .catch(err => console.log(`OHMYGOD: ${err}`));
+  }
 
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Dashboard products={this.state.products} displayInventory={this.displayInventory} selectedProduct={this.selectedProduct} />
-        <Product />
-        <Form displayInventory={this.displayInventory} editId={this.state.editId} />
-      </div>
+    
+        <div className="App">
+          <Header />
+          <Dashboard
+            products={this.state.products}
+            displayInventory={this.displayInventory}
+            selectedProduct={this.selectedProduct}
+          />
+          <Form
+            displayInventory={this.displayInventory}
+            editId={this.state.editId}
+          />
+        </div>
+      
     );
   }
 }
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class Product extends Component {
   render() {
-      let { name, price, imgURL, id } = this.props;
+      let { name, price, imgURL, id, deleteProduct} = this.props;
     return (
       <div>
           <div>
@@ -10,8 +10,8 @@ class Product extends Component {
               <img src={imgURL} alt="" width={250} />
               <h2>{name}</h2>
               <h2>{price}</h2>
-              <button onClick={()=>{this.selecetedProduct(id)}}>edit</button>
-              <button onClick={()=>{this.props.deleteProduct(id)}}>delete</button>
+              <button onClick={()=>{this.selectedProduct(id)}}>edit</button>
+              <button onClick={()=>{deleteProduct(id)}}>delete</button>
           </div>
       </div>
     )
